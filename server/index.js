e7ede777ac59ce2ef27abd6b6e44c566a1e90c67
DanTayar <dantayar28@gmail.com>
2019-07-02 14:53:13 +0300

    added me vote model

--- server/index.js
+++ server/index.js
@@ -2,11 +2,43 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 
+const ORM = require('sequelize');
+
+const connection = new ORM('postgres://memewars:password@localhost:5432/memewars');
+
+const modelsFactory = require('./models');
+const {User , Meme , Vote} = modelsFactory (connection , ORM);
+
+connection.authenticate()
+	.then(() => console.log('success'))
+	.catch((err) => console.log(err));
+
+
+
 app.use( express.static('build') );
 app.use( express.json() );
 
 
+app.get('/hydrate', (req, res)=> {
+  // sync table
+  	User.sync({ force: true })
+  	.then(() => Meme.sync({ force: true }))
+  	.then(() => Vote.sync({ force: true }))
+  	.then(()=> res.json({ message: 'success creating User , Meme , Vote table' }))
+  		.catch(err => res.status(500).json({message: JSON.stringify(err)}))
+});
+
 // connect to db
+
+app.post('/User', (req, res)=> {
+	User.create(req.body)
+		.then(UserCreateResponse => console.log(UserCreateResponse))
+		.then(() => res.json({message: 'user created' })) 
+		.catch(err => {
+			console.error(err);
+			res.status(500).json({ message: JSON.stringify(err) })
+	});
+});
 
 
 app.post('/meme', (req, res)=> {
